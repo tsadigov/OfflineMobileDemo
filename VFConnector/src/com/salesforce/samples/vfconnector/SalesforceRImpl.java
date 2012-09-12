223f46b40df15412ba47079f36f76f6e4ccb17d0
kemalyildiz <kyildiz@atlantic-technologies.com>
2012-09-12 19:32:05 +0300

    aaa

--- VFConnector/src/com/salesforce/samples/vfconnector/SalesforceRImpl.java
+++ VFConnector/src/com/salesforce/samples/vfconnector/SalesforceRImpl.java
@@ -24,6 +24,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+ //YASASIN PASAPORT
 package com.salesforce.samples.vfconnector;
 
 import com.salesforce.androidsdk.ui.SalesforceR;
